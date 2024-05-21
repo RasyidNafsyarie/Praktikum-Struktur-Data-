69f0b4c514b8e4c708c2a7388d3289207e0da0e1
RasyidNafsyarie <rasyidnafsyarie@gmail.com>
2024-05-21 18:30:12 +0700

    [RN] feat:menambahkan semua modul

---
+++ Modul 6/unguided2.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <stack>
+#include <sstream>
+#include <string>
+using namespace std;
+
+string reverseWords(string sentence) {
+    stack<char> charStack;
+    stringstream ss(sentence);
+    string word;
+    string reversedSentence = "";
+
+    while (ss >> word) {
+        for (char c : word) {
+            charStack.push(c);
+        }
+
+        while (!charStack.empty()) {
+            reversedSentence += charStack.top();
+            charStack.pop();
+        }
+
+        reversedSentence += ' '; 
+    }
+    if (!reversedSentence.empty()) {
+        reversedSentence.pop_back();
+    }
+
+    return reversedSentence;
+}
+int main() {
+    string sentence;
+
+    cout << "Masukkan kalimat (minimal 3 kata): ";
+    getline(cin, sentence);
+
+    int wordCount = 0;
+    stringstream ss(sentence);
+    string word;
+    while (ss >> word) {
+        wordCount++;
+    }
+
+    if (wordCount < 3) {
+        cout << "Kalimat harus terdiri dari minimal 3 kata." << endl;
+    } else {
+        string result = reverseWords(sentence);
+        cout << "Data: " << result << endl;
+    }
+
+    return 0;
+}
